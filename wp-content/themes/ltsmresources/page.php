aac488cd7b93b0eda1e2089b3c9d5f9e51de6575
Khomotso Mkansi <kgomotsomkansi@gmail.com>
2023-06-11 11:45:48 +0200

    files

--- wp-content/themes/ltsmresources/page.php
+++ wp-content/themes/ltsmresources/page.php
@@ -3,9 +3,7 @@ get_header();
 
     while(have_posts()) {
         the_post(); ?>
-        <h1>pag</h1>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+        
         
     <?php }
 
