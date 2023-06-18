4801ef2722f1125a8e0eb955353408710bf26068
momotsow <kgomotsomkansi@gmail.com>
2023-06-18 17:21:03 +0200

    files

--- wp-content/mu-plugins/ltsm-post-types.php
+++ wp-content/mu-plugins/ltsm-post-types.php
@@ -33,6 +33,21 @@ function ltsm_post_types() {
       ),
       'menu_icon' => 'dashicons-awards'
     ));
+
+     // Professor Post Type
+    register_post_type('professor', array(
+      'show_in_rest' => true,
+      'supports' => array('title', 'editor', 'thumbnail'),
+      'public' => true,
+      'labels' => array(
+        'name' => 'Professors',
+        'add_new_item' => 'Add New Professor',
+        'edit_item' => 'Edit Professor',
+        'all_items' => 'All Professors',
+        'singular_name' => 'Professor'
+      ),
+      'menu_icon' => 'dashicons-welcome-learn-more'
+    ));
 }
 
 add_action('init', 'ltsm_post_types');
--- wp-content/themes/ltsmresources/functions.php
+++ wp-content/themes/ltsmresources/functions.php
@@ -11,25 +11,33 @@ add_action('wp_enqueue_scripts', 'ltsm_resources_files');
 
 function resources_feature() {
     add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'resources_feature');
 
 function ltsm_adjust_queries($query) {
-    if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
-      $today = date('Ymd');
-      $query->set('meta_key', 'event_date');
-      $query->set('orderby', 'meta_value_num');
-      $query->set('order', 'ASC');
-      $query->set('meta_query', array(
-                array(
-                  'key' => 'event_date',
-                  'compare' => '>=',
-                  'value' => $today,
-                  'type' => 'numeric'
-                )
-              ));
-    }
+  if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
   }
+
+  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
   
-  add_action('pre_get_posts', 'ltsm_adjust_queries');
+add_action('pre_get_posts', 'ltsm_adjust_queries');
